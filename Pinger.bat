8f4bacd879310adaf2bd145ac58d98af44791f97
AidanTay005 <40963590+AidanTay005@users.noreply.github.com>
2018-07-08 20:02:34 +0200

    Add files via upload
    This is a pinger, you chose whether you want only 4 ping requests or have it go endlessly.

---
+++ Pinger.bat
@@ -0,0 +1,41 @@
+@echo off 
+title Pinger
+:start
+cls 
+echo. 
+color 07
+echo               MENU 
+echo --------------------------------
+echo 1 = Ping a IP Address For 4 Times
+echo 2 = Ping a IP Address Endlessly
+
+echo. 
+set /p opt= Option: 
+if %opt%==1 goto PING 
+if %opt%==2 goto PING2
+
+cls 
+pause 
+goto start 
+
+
+:PING
+color 03
+cls & set /p adress=Type an adress you wish to ping: 
+cls
+
+ping %adress% & echo. & pause. & goto :start
+
+
+:PING2
+cls
+color 03
+set /P CommandVar=Type an adress you wish to ping:  
+ping -t %CommandVar%
+color 07
+goto start 
+
+:3 
+echo Rofl
+pause 
+goto start
